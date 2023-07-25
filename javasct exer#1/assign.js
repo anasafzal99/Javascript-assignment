4360919d7f2a9f7ea7eae36dd91b8074f7ed4c5d
anasafzal99 <137061145+anasafzal99@users.noreply.github.com>
2023-07-25 17:00:36 +0500

    Add files via upload

---
+++ javasct exer#1/assign.js
@@ -0,0 +1,158 @@
+// var firstName = prompt("what is your first name? :");
+// var secondName = prompt("what is your second name? :");
+// var fullName = firstName + secondName;
+// console.log("hey", fullName);
+
+//////////////
+
+
+// var input = prompt("What's your favourite Smartphone", "iphone 12 pro" );
+// var result = input.length;
+
+// console.log("Phone is  " +  input + " and size is  " + result );
+
+
+
+////////////////
+
+// var country = prompt("Pakistan", "Pakistan");
+// var result = country.indexOf("n");
+
+// console.log("Country: " + country + " index of 'n': " + result);
+
+
+//////////////
+
+// var input = prompt("Hello World", "Hello World");
+// var result = input.lastIndexOf("l");
+// console.log("Sring: " + input + " Last index of 'l'" + result )
+
+
+/////////////////
+
+// var input = prompt("Pakistan", "Pakistan");
+// var result = input.indexOf("i", 3);
+// console.log("String: " + input + " Index of '3rd'" + result );
+
+
+///////////////
+
+
+    // var originalWord = "Hyderabad";
+    // var replacedWord = originalWord.replace("Hyder", "Islam");
+    // console.log("Original Word: " + originalWord + " Replaced word: " + replacedWord);
+  
+
+/////////////////////
+
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var replaced = message.replace("and", "&");
+// console.log("Original Message:" + message + " Replaced:" + replaced);
+
+
+//////////////////////////
+
+// var str = "472";
+// var change = Number(str);
+// console.log("Value: " + str + " Type: String" + " Value: " + change + " Type: Number"); 
+
+
+
+
+
+////////////////////
+
+// var userInput = prompt("Ener URL", "www.google.com");
+// var domainName = userInput.slice(4);
+// console.log("URL: " + userInput + "Domain: " + domainName);
+
+
+
+/////////////////////////////
+
+
+// var lower = "peanuts";
+// var upper = lower.toUpperCase();
+// console.log("Lower: " + lower + " Upper: " + upper);
+
+///////////////////
+
+// var upper = "Anas";
+// var lower = upper.toLowerCase();
+// console.log("Upper: " + upper + " Lower: " + lower);
+
+///////////////////////
+
+// var userInput = prompt("Enter your word", "javascript");
+// var firstChar = userInput.slice(0,1);
+// var remainingChar = userInput.slice(1);
+// firstChar = firstChar.toUpperCase();
+// remainingChar = remainingChar.toLowerCase();
+// var title = firstChar + remainingChar;
+// console.log("UserInput " + userInput + " TitleCase " + title);
+
+
+
+////////////////////
+
+
+// var num = "35.36";
+// var str = num.replace(".", "");
+// console.log ("Number: " + num + " Result: " + str); 
+
+
+/////////////////////////
+
+
+// var a = 3;
+// var b = 3;
+// var result = a + b;
+// console.log("A is " + a + " B is " + b + " A+B is " + result);
+
+
+/////////////////////////////
+
+// var a = 3;
+// var b = 3;
+// var y = a - b;
+// console.log("A is " + a + " B is " + b + " A-B is " + y);
+
+
+//////////////////////////////////
+
+// function searchItem(array, userInput) {
+//     // Convert the user input and array items to lowercase for case-insensitive search
+//     const lowerUserInput = userInput.toLowerCase();
+//     const lowerArray = array.map(item => item.toLowerCase());
+  
+//     // Check if the lowercased user input exists in the lowercased array
+//     const found = lowerArray.includes(lowerUserInput);
+  
+//     // Prompt the user about the availability of the item
+//     if (found) {
+//       console.log('Item found in the list.');
+//     } else {
+//       console.log('Item not found in the list.');
+//     }
+//   }
+  
+//   // Example usage
+//   const A = ["cake", "apple pie", "cookie", "chips", "patties"];
+//   const userInput = prompt('Enter an item to search:');
+  
+//   searchItem(A, userInput);
+
+
+////////////////////////////
+
+// var university = "University of Karachi";
+// var uniee = university.split(" ");
+// console.log(uniee);
+
+//////////////////////
+
+// var input = "Pakistan";
+// var result = "'n'";
+// console.log("Input: " + input + " last word is:" + result );
+  
+
